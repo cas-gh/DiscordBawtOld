b6b9f3512754d52d6e3fdc125211b108b460c7b9
orangeismyfavorite <xsoltis@gmail.com>
2021-01-17 17:02:28 -0500

    Added role check to message handling.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # This is a Discord bot that allows for more IRC-like moderation
 # TODO:
-# 1) Implement actual functionality to mute and unmute after time passes
-# 2) Implement a totally arbitrary point system that people can assign to others
-# 3) Only allow certain roles to use certain commands (moderator, verified, etc.)
+# 1) Implement actual functionality to mute and unmute after time passes.
+# 2) Create database for the point system.
 
 import discord
+from discord.utils import get
 import config
 
 # Sets the intents on startup so bot can read all users in the server
@@ -28,24 +28,28 @@ async def on_message(message):
 
     # Ridiculous slice picks out just the discord username from messy generator
     # and forces lower case for ease of comparison later
-    if message.content.startswith(',mute'):
+    if message.content.startswith(',mute') and get(message.author.roles, name="Moderator"):
         member_list = [str(i).split('#')[0].lower() for i in client.get_all_members()]
 
         # Try/Except block checking for ValueErrors and valid mute command
         try:
             if message.content[5] == ' ':
                 raise ValueError
+
             elif message.content[6] == ' ':
                 mute_time = int(message.content[5])
                 mute_member = message.content[7:].lower()
+
             else:
                 mute_time = int(message.content[5:7])
                 mute_member = message.content[8:].lower()
 
             if mute_time > 60 or mute_time < 1:
                 await message.channel.send("That is not a valid temporary mute time.")
+
             elif mute_member not in member_list:
                 await message.channel.send("There is no user in this server by that name.")
+
             else:
                 await message.channel.send(f'Muted {mute_member} for {mute_time} minutes.')
 
@@ -54,38 +58,41 @@ async def on_message(message):
 
     # Voting system
     if message.content.startswith('++') or message.content.startswith('--'):
-        # Silly slice to get all server members
-        member_list = [str(i).split('#')[0].lower() for i in client.get_all_members()]
-        # Index between command/vote and given username
-        space_index = message.content.find(' ')
-        vote_sign = message.content[:2]
-        vote_amount = message.content[2:space_index]
-        vote_member = message.content[space_index + 1:]
+        if get(message.author.roles, name="Registered Voter"):
+            # Silly slice to get all server members
+            member_list = [str(i).split('#')[0].lower() for i in client.get_all_members()]
+            # Index between command/vote and given username
+            space_index = message.content.find(' ')
+            vote_sign = message.content[:2]
+            vote_amount = message.content[2:space_index]
+            vote_member = message.content[space_index + 1:]
 
-        # Try/Except block that ensures valid input
-        try:
-            vote_amount = int(vote_amount)
+            # Try/Except block that ensures valid input
+            try:
+                vote_amount = int(vote_amount)
 
-            if vote_member not in member_list:
-                await message.channel.send("There is no user in this server by that name.")
+                if vote_member not in member_list:
+                    await message.channel.send("There is no user in this server by that name.")
 
-            elif vote_sign == '--':
-                if vote_amount < 1:
-                    raise ValueError
+                elif vote_sign == '--':
+                    if vote_amount < 1:
+                        raise ValueError
+                    else:
+                        await message.channel.send(f'Vote amount: -{vote_amount}.\nVoted for: {vote_member}.')
+
+                elif vote_sign == '++':
+                    if vote_amount < 1:
+                        raise ValueError
+                    else:
+                        await message.channel.send(f'Vote amount: {vote_amount}.\nVoted for: {vote_member}.')
+
                 else:
-                    await message.channel.send(f'Vote amount: -{vote_amount}.\nVoted for: {vote_member}.')
+                    await message.channel.send("Something went wrong.")
 
-            elif vote_sign == '++':
-                if vote_amount < 1:
-                    raise ValueError
-                else:
-                    await message.channel.send(f'Vote amount: {vote_amount}.\nVoted for: {vote_member}.')
-
-            else:
-                await message.channel.send("Something went wrong.")
-
-        except ValueError:
-            await message.channel.send("Invalid vote amount.")
+            except ValueError:
+                await message.channel.send("Invalid vote amount.")
+        else:
+            await message.channel.send("You don't have permission to do that.")
 
 
 if __name__ == "__main__":
