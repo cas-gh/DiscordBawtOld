886e8dcb00f25265733feeb432c83240e77e2504
orangeismyfavorite <xsoltis@gmail.com>
2021-01-17 15:14:31 -0500

    Added beginning of point system

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # TODO:
 # 1) Implement actual functionality to mute and unmute after time passes
 # 2) Implement a totally arbitrary point system that people can assign to others
+# 3) Only allow certain roles to use certain commands (moderator, verified, etc.)
 
 import discord
 import config
@@ -50,6 +51,41 @@ async def on_message(message):
 
         except ValueError:
             await message.channel.send("Invalid Command.")
+
+    # Voting system
+    if message.content.startswith('++') or message.content.startswith('--'):
+        # Silly slice to get all server members
+        member_list = [str(i).split('#')[0].lower() for i in client.get_all_members()]
+        # Index between command/vote and given username
+        space_index = message.content.find(' ')
+        vote_sign = message.content[:2]
+        vote_amount = message.content[2:space_index]
+        vote_member = message.content[space_index + 1:]
+
+        # Try/Except block that ensures valid input
+        try:
+            vote_amount = int(vote_amount)
+
+            if vote_member not in member_list:
+                await message.channel.send("There is no user in this server by that name.")
+
+            elif vote_sign == '--':
+                if vote_amount < 1:
+                    raise ValueError
+                else:
+                    await message.channel.send(f'Vote amount: -{vote_amount}.\nVoted for: {vote_member}.')
+
+            elif vote_sign == '++':
+                if vote_amount < 1:
+                    raise ValueError
+                else:
+                    await message.channel.send(f'Vote amount: {vote_amount}.\nVoted for: {vote_member}.')
+
+            else:
+                await message.channel.send("Something went wrong.")
+
+        except ValueError:
+            await message.channel.send("Invalid vote amount.")
 
 
 if __name__ == "__main__":
